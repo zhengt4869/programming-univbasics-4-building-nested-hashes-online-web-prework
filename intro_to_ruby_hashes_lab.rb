e28b914054fcf22379637b657c04c3a4a2e99dce
Tianlin Zheng <zhengt@mit.edu>
2019-08-09 15:49:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  {:railroads =>{:pieces =>}}
+  {:railroads =>{:pieces => 4}}
 end
 
 def monopoly_with_third_tier
